61db51fa5daf1d6a8f3ae5eb00bfc57908b17536
jevans63 <jevans63@kent.edu>
2020-10-18 16:56:31 -0400

    initial functionality

---
+++ PuzzleGame3D/Assets/Scripts/CubeSpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawn : MonoBehaviour
+{
+    public GameObject cube;
+    public Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating ("Spawn", 3.0f, 3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        Instantiate(cube, spawnPoint.position, spawnPoint.rotation);
+    }
+}
