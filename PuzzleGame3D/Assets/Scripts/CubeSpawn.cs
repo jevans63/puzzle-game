35ba0d7bb4b17394e414b89915fb13f4d6825d3a
jevans63 <jevans63@kent.edu>
2020-11-25 13:15:25 -0500

    adding more columns and gameplay tweaks

--- PuzzleGame3D/Assets/Scripts/CubeSpawn.cs
+++ PuzzleGame3D/Assets/Scripts/CubeSpawn.cs
@@ -6,8 +6,8 @@ public class CubeSpawn : MonoBehaviour
 {
     public GameObject[] cube;
     public Transform[] spawnPoint;
-    public float timeToBegin;
-    public float timeBetween;
+    public float timeToBegin = 1f;
+    public float timeBetween = 1f;
     
     void Start()
     {
@@ -17,8 +17,25 @@ public class CubeSpawn : MonoBehaviour
     void Spawn()
     {
         int cubeIndex = Random.Range (0, cube.Length);
+        int cubeIndex2 = Random.Range (0, cube.Length);
+        int cubeIndex3 = Random.Range (0, cube.Length);
         int spawnIndex = Random.Range (0, spawnPoint.Length);
+        int spawnIndex2 = Random.Range (0, spawnPoint.Length);
+        int spawnIndex3 = Random.Range (0, spawnPoint.Length);
+
         Instantiate(cube[cubeIndex], spawnPoint[spawnIndex].position, spawnPoint[spawnIndex].rotation);
-        timeBetween += 0.01f;
+
+        if(spawnIndex2 != spawnIndex && spawnIndex2 != spawnIndex3)
+        {
+            Instantiate(cube[cubeIndex2], spawnPoint[spawnIndex2].position, spawnPoint[spawnIndex2].rotation);
+        }
+
+        if(spawnIndex3 != spawnIndex && spawnIndex3 != spawnIndex2)
+        {
+            Instantiate(cube[cubeIndex3], spawnPoint[spawnIndex3].position, spawnPoint[spawnIndex3].rotation);
+        }
+        
+        timeToBegin -= timeToBegin * .001f;
+        timeBetween -= timeBetween * .001f;
     }
 }
