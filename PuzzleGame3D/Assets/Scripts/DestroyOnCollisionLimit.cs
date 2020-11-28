286f54b5fda9bb7df120ee4223f4e7260a3f22ec
jevans63 <jevans63@kent.edu>
2020-11-28 17:35:17 -0500

    adding particle system to main menu and game over text

--- PuzzleGame3D/Assets/Scripts/DestroyOnCollisionLimit.cs
+++ PuzzleGame3D/Assets/Scripts/DestroyOnCollisionLimit.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class DestroyOnCollisionLimit : MonoBehaviour
 {        
+    public GameObject gameOverText;
     void OnCollisionEnter(Collision col)
     {
         Destroy(col.gameObject);
+        StartCoroutine(GameOver(3));
+    }
+
+    IEnumerator GameOver(float time)
+    {
+        gameOverText.SetActive(true);
+        yield return new WaitForSeconds(time);
         SceneManager.LoadScene("MainMenu");
     }
 }
