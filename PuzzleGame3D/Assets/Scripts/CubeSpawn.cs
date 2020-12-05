39227dbf6ecb4f8dd5a84fcee940fee99b6550c1
jevans63 <jevans63@kent.edu>
2020-12-05 18:36:12 -0500

    Orange blocks made to spawn after delay

--- PuzzleGame3D/Assets/Scripts/CubeSpawn.cs
+++ PuzzleGame3D/Assets/Scripts/CubeSpawn.cs
@@ -11,7 +11,32 @@ public class CubeSpawn : MonoBehaviour
     
     void Start()
     {
-        InvokeRepeating ("Spawn", timeToBegin, timeBetween);
+        InvokeRepeating ("SpawnInitial", timeToBegin, timeBetween);
+        StartCoroutine(BombPhase(15));
+    }
+        void SpawnInitial()
+    {
+        int cubeIndex = Random.Range (0, cube.Length-1);
+        int cubeIndex2 = Random.Range (0, cube.Length-1);
+        int cubeIndex3 = Random.Range (0, cube.Length-1);
+        int spawnIndex = Random.Range (0, spawnPoint.Length);
+        int spawnIndex2 = Random.Range (0, spawnPoint.Length);
+        int spawnIndex3 = Random.Range (0, spawnPoint.Length);
+
+        Instantiate(cube[cubeIndex], spawnPoint[spawnIndex].position, spawnPoint[spawnIndex].rotation);
+
+        if(spawnIndex2 != spawnIndex && spawnIndex2 != spawnIndex3)
+        {
+            Instantiate(cube[cubeIndex2], spawnPoint[spawnIndex2].position, spawnPoint[spawnIndex2].rotation);
+        }
+
+        if(spawnIndex3 != spawnIndex && spawnIndex3 != spawnIndex2)
+        {
+            Instantiate(cube[cubeIndex3], spawnPoint[spawnIndex3].position, spawnPoint[spawnIndex3].rotation);
+        }
+        
+        timeToBegin -= timeToBegin * .001f;
+        timeBetween -= timeBetween * .001f;
     }
 
     void Spawn()
@@ -37,5 +62,10 @@ public class CubeSpawn : MonoBehaviour
         
         timeToBegin -= timeToBegin * .001f;
         timeBetween -= timeBetween * .001f;
+    }
+    IEnumerator BombPhase(int time)
+    {
+        yield return new WaitForSeconds(time);
+        InvokeRepeating ("Spawn", timeToBegin, timeBetween);
     }
 }
